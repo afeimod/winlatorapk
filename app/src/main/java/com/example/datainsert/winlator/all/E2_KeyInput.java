8955e9bb31ad7affde7365c7a3f95e8c5e7b7c7e
ewt45 <79033456+ewt45@users.noreply.github.com>
2024-04-11 17:38:24 +0800

    旧功能：修复unicode输入后 对应keycode无法再输入原本的keysym

--- app/src/main/java/com/example/datainsert/winlator/all/E2_KeyInput.java
+++ app/src/main/java/com/example/datainsert/winlator/all/E2_KeyInput.java
@@ -1,10 +1,19 @@
 package com.example.datainsert.winlator.all;
 
+import static com.winlator.xserver.Keyboard.KEYSYMS_PER_KEYCODE;
+
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 import android.view.KeyEvent;
 
+import com.winlator.xserver.Bitmask;
+import com.winlator.xserver.Window;
 import com.winlator.xserver.XKeycode;
+import com.winlator.xserver.XLock;
 import com.winlator.xserver.XServer;
+import com.winlator.xserver.events.Event;
+import com.winlator.xserver.events.MappingNotify;
 
 public class E2_KeyInput {
     private static final String TAG = "E2_KeyInput";
@@ -19,22 +28,64 @@ public class E2_KeyInput {
      * 此函数只处理KeyEvent.ACTION_MULTIPLE的情况
      */
     public static boolean handleAndroidKeyEvent(XServer xServer, KeyEvent event){
+
         boolean handled = false;
         if(event.getAction() == KeyEvent.ACTION_MULTIPLE){
             String characters = event.getCharacters();
             for (int i = 0; i < characters.codePointCount(0, characters.length()); i++) {
-                //大于0xff的，直接加上0x100,0000
+                int keycode = avaiKeyCode[currIndex].id;
                 int keySym = characters.codePointAt(characters.offsetByCodePoints(0, i));
-                if(keySym>0xff)
-                    keySym = keySym | 0x1000000;
+                //大于0xff的，直接加上0x100,0000
+                if(keySym>0xff) keySym = keySym | 0x1000000;
+
+                int[] oriKeySyms = getCurrentKeysym(xServer, keycode);
+
                 xServer.injectKeyPress(avaiKeyCode[currIndex], keySym);
                 xServer.injectKeyRelease(avaiKeyCode[currIndex]);
+
+                //手动把keycode对应的unicode keysym改回来。直接Thread.sleep还不行，只能postDelay了
+                int[] nowKeySyms = getCurrentKeysym(xServer, keycode);
+                if(oriKeySyms[0] != nowKeySyms[0] || oriKeySyms[1] != nowKeySyms[1])
+                    new Handler(Looper.getMainLooper()).postDelayed(()-> mappingKeySymBackToOrigin(xServer, (byte) keycode, oriKeySyms), 30);
+                
                 currIndex = (currIndex+1)%avaiKeyCode.length;//数组下标+1，为下一次设置另一个keycode做准备
                 handled = true;
             }
             Log.d(TAG, "handleAKeyEvent: action=multiple, string="+characters);
         }
         return handled;
+    }
 
+    private static int[] getCurrentKeysym(XServer xServer, int keycode){
+        int keysymIdxStart = (keycode-8)*KEYSYMS_PER_KEYCODE;
+        int[] keyboardKeysyms = xServer.keyboard.keysyms;
+        return new int[]{keyboardKeysyms[keysymIdxStart],keyboardKeysyms[keysymIdxStart+1]};
+    }
+
+    /**
+     * injectKeyPress最终会走到InputDeviceManager.onKeyPress，
+     * 如果xServer.keyboard所存的keycode对应的keysym不是传入的，且keysym不为0，则将其设置为此keycode的keysym，并发送MappingNotify
+     * 然后后续没有恢复原先keycode的操作。所以这里要手动模拟一下咯
+     * @param keycode 输入unicode按下事件时对应的keycode
+     * @param oriKeySyms keycode原先的两个keysym
+     */
+    private static void mappingKeySymBackToOrigin(XServer xServer, byte keycode, int[] oriKeySyms){
+        try (XLock lock = xServer.lock(XServer.Lockable.WINDOW_MANAGER, XServer.Lockable.INPUT_DEVICE)) {
+            Window focusedWindow = xServer.windowManager.getFocusedWindow();
+            if (focusedWindow == null) return;
+            Window pointWindow = xServer.windowManager.findPointWindow(xServer);
+
+            Window eventWindow;
+            if (focusedWindow.isAncestorOf(pointWindow)) {
+                eventWindow = pointWindow.getAncestorWithEventId(Event.KEY_PRESS, focusedWindow);
+            }else if (!focusedWindow.hasEventListenerFor(Event.KEY_PRESS))
+                eventWindow = focusedWindow;
+            else return;
+
+            if (!eventWindow.attributes.isEnabled()) return;
+
+            xServer.keyboard.setKeysyms(keycode, oriKeySyms[0], oriKeySyms[1]);
+            eventWindow.sendEvent(new MappingNotify(MappingNotify.Request.KEYBOARD, keycode, 1));
+        }
     }
 }
