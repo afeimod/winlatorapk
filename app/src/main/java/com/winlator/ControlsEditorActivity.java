e9b8bf8928889fdafd593bc8db8103a0771aedf0
afei <165532911+afeimod@users.noreply.github.com>
2024-05-14 20:31:58 +0800

    Add files via upload

--- app/src/main/java/com/winlator/ControlsEditorActivity.java
+++ app/src/main/java/com/winlator/ControlsEditorActivity.java
@@ -91,12 +91,14 @@ public class ControlsEditorActivity extends AppCompatActivity implements View.On
             ControlElement.Type type = element.getType();
             view.findViewById(R.id.LLShape).setVisibility(View.GONE);
             view.findViewById(R.id.CBToggleSwitch).setVisibility(View.GONE);
+            view.findViewById(R.id.CBToggleVibrate).setVisibility(View.GONE);//振动
             view.findViewById(R.id.LLCustomTextIcon).setVisibility(View.GONE);
             view.findViewById(R.id.LLRangeOptions).setVisibility(View.GONE);
 
             if (type == ControlElement.Type.BUTTON) {
                 view.findViewById(R.id.LLShape).setVisibility(View.VISIBLE);
                 view.findViewById(R.id.CBToggleSwitch).setVisibility(View.VISIBLE);
+                view.findViewById(R.id.CBToggleVibrate).setVisibility(View.VISIBLE);//振动
                 view.findViewById(R.id.LLCustomTextIcon).setVisibility(View.VISIBLE);
             }
             else if (type == ControlElement.Type.RANGE_BUTTON) {
@@ -153,6 +155,14 @@ public class ControlsEditorActivity extends AppCompatActivity implements View.On
         cbToggleSwitch.setChecked(element.isToggleSwitch());
         cbToggleSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
             element.setToggleSwitch(isChecked);
+            profile.save();
+        });
+
+        //振动
+        CheckBox cbToggleVibrate = view.findViewById(R.id.CBToggleVibrate);
+        cbToggleVibrate.setChecked(element.vibrate);
+        cbToggleVibrate.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            element.vibrate = isChecked;
             profile.save();
         });
 
